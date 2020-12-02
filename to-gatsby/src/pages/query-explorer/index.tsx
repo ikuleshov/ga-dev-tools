049bbb80518e9acf9e405f6f28a0c26fd8b29148
Matt Hamrick <matthewjhamrick@gmail.com>
2020-12-02 19:21:56 +0000

    Add in example for how state mangaement works with hooks.

--- to-gatsby/src/pages/query-explorer/index.tsx
+++ to-gatsby/src/pages/query-explorer/index.tsx
@@ -15,8 +15,53 @@
 import * as React from "react"
 
 import Layout from "../../components/layout"
+import ViewSelector, { HasView } from "../../components/ViewSelector"
 
 const QueryExplorer = () => {
-  return <Layout title="Query Explorer">TODO - add in content.</Layout>
+  const [ids, setIds] = React.useState<string>("")
+  const [summary, setView] = React.useState<HasView | undefined>(undefined)
+  const [queryResults, setQueryResults] = React.useState<string>("")
+
+  // When the summary is updated, update the selected ids
+  React.useEffect(() => {
+    if (summary !== undefined) {
+      const view = summary.view
+      if (view !== undefined) {
+        const id = view.id
+        if (id !== undefined) {
+          setIds(id)
+        }
+      }
+    }
+    // The array [summary] tells react to re-run the function passed to
+    // useEffect anytime the "summary" variable changes.
+  }, [summary])
+
+  const runQuery = React.useCallback(() => {
+    // Wait for 3 seconds because we're pretending like this is a web call
+    setTimeout(() => {
+      setQueryResults(`You called the function with this id: ${ids}`)
+    }, 3000)
+  }, [ids])
+
+  return (
+    <Layout title="Query Explorer">
+      <ViewSelector onViewChanged={setView} />
+      <button onClick={runQuery}>Run Query</button>
+      <label>Ids:</label>
+      <input
+        className="FormField FormFieldCombo-field"
+        name="ids"
+        value={ids}
+        onChange={event => {
+          // If the user manually updates this value, overwrite anything that
+          // the viewSelector returned.
+          setIds(event.target.value)
+        }}
+      />
+      <br />
+      <div>{queryResults}</div>
+    </Layout>
+  )
 }
 export default QueryExplorer
